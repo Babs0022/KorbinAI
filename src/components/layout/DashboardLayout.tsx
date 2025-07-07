4304d9cb470a2f5deee68e7411f38cd474f7d49d
Babatunde Elijah <babseli933@gmail.com>
2025-07-07 08:43:07 +0000

    Add a footer

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from 'react';
 import DashboardHeader from './DashboardHeader';
+import Footer from './Footer';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <DashboardHeader />
       {children}
+      <Footer />
     </div>
   );
 }
