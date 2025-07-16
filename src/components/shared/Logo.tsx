563cfa22f343ddc67f3cb7ea57df1f883050a2a1
Babatunde Elijah <babseli933@gmail.com>
2025-07-16 22:50:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <span class="text-

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,11 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface LogoProps {
-  showText?: boolean;
-}
-
-const Logo = ({ showText = true }: LogoProps) => {
+const Logo = () => {
   return (
     <Link href="/" className="flex items-center space-x-2">
       <Image
@@ -15,7 +11,6 @@ const Logo = ({ showText = true }: LogoProps) => {
         height={32}
         className="h-8 w-8"
       />
-      {showText && <span className="text-xl font-bold">BrieflyAI</span>}
     </Link>
   );
 };
