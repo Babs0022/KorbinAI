532644c57df557c5195d49e4969ecba1b246ab79
Babatunde Elijah <babseli933@gmail.com>
2025-08-02 16:52:34 +0000

    rror loading webview: Error: Could not register service worker: AbortErr

--- src/next.config.ts
+++ src/next.config.ts
@@ -5,6 +5,12 @@ import type {NextConfig} from 'next';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+});
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -59,4 +65,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
