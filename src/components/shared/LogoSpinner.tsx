0dc6166569555c5cb6ad7e6a41c1e70a8a6db3cc
Babatunde Elijah <babseli933@gmail.com>
2025-08-13 17:17:07 +0000

    now that we have the "thinking..." we should remove that spinner around

--- src/components/shared/LogoSpinner.tsx
+++ src/components/shared/LogoSpinner.tsx
@@ -11,8 +11,6 @@ interface LogoSpinnerProps {
 const LogoSpinner = ({ className }: LogoSpinnerProps) => {
     return (
         <div className={cn("relative flex items-center justify-center w-12 h-12", className)}>
-            <div className="absolute inset-0 border-4 border-primary/20 rounded-full"></div>
-            <div className="absolute inset-0 border-t-4 border-t-primary rounded-full animate-spin-slow"></div>
             <Logo />
         </div>
     );
